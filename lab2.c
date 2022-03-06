e91911346cc8f8c8e0184e904876c3c481afd00c
K-Alena-S <97899604+K-Alena-S@users.noreply.github.com>
2022-03-06 19:24:40 +0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 #define ERROR_TIME -1
-#define TRUE_SETENV 0
+#define ERROR_SETENV -1
 #define ERROR 1
 #define SUCCESSFUL_END 0
 
@@ -15,32 +15,32 @@ int main(){
   struct tm *struct_pointer; 
   char *Ctime;
   
-  int result_time = time(&now);
+  int result_time = time(&now); // текущее календарное значение времени в секундах сохраняется по адресу now, отсчет секунд с 01.01.1970 по Гринвичу (время Лондона)
   if (result_time == ERROR_TIME){
     perror("Error");
     return ERROR;
   }
   
-  int result_setenv = setenv("TZ", "PST8PDT", 1);
-  if (result_setenv != TRUE_SETENV){
+  int result_setenv = setenv("TZ", "PST8PDT", 1); // копирует переменную окружения из оболочки bash и меняет часовой пояс на Калифорнию, 
+  if (result_setenv == ERROR_SETENV){                                                                      // основная (откуда копируются) переменная не изменяется 
     perror("Error");
     return ERROR;
   }
   
-  Ctime = ctime(&now);
+  Ctime = ctime(&now); // преобразует время в секундах, в дату в понятной форме
   if (Ctime == NULL){
     perror("Error");
     return ERROR;
   }
-  printf("%s", Ctime);
+  printf("%s", Ctime); // пример вывода: Sun Mar  6 04:15:48 2022 (день недели/ месяц/ день месяца/ время/ год)
   
-  struct_pointer = localtime(&now);
+  struct_pointer = localtime(&now); //  Время указанное в now используется для заполнения структуры tm значениями текущей даты и времени.
   if (struct_pointer == NULL){
     perror("Error");
     return ERROR;
   }
   
-  printf("%02d/%d/%02d %02d:%02d:%02d %s\n",
+  printf("%02d/%d/%02d %02d:%02d:%02d %s\n", // пример вывода: 03/6/2022 04:15:48 PST (месяц/ день/ год/ время/ часовой пояс)
          struct_pointer->tm_mon + 1 , struct_pointer->tm_mday,
          struct_pointer->tm_year+1900, struct_pointer->tm_hour,
          struct_pointer->tm_min, struct_pointer->tm_sec, tzname[struct_pointer->tm_isdst]);
