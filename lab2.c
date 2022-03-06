3fb79523ab40a02dd3ea6f8fa96c35c301dee020
K-Alena-S <97899604+K-Alena-S@users.noreply.github.com>
2022-03-06 16:02:27 +0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -3,43 +3,46 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define error_time -1
-#define true_setenv 0
-#define error NULL
+#define ERROR_TIME -1
+#define TRUE_SETENV 0
+#define ERROR 1
+#define SUCCESSFUL_END 0
 
 extern char *tzname[];
 
 int main(){
   time_t now;
-  struct tm *sp;
+  struct tm *struct_pointer; 
   char *Ctime;
   
   int result_time = time(&now);
-  if (result_time == error_time){
+  if (result_time == ERROR_TIME){
     perror("Error");
-    return 1;
+    return ERROR;
   }
   
   int result_setenv = setenv("TZ", "PST8PDT", 1);
-  if (result_setenv != true_setenv){
+  if (result_setenv != TRUE_SETENV){
     perror("Error");
-    return 1;
+    return ERROR;
   }
   
-  if ((Ctime = ctime(&now)) == error){
+  Ctime = ctime(&now);
+  if (Ctime == NULL){
     perror("Error");
-    return 1;
+    return ERROR;
   }
   printf("%s", Ctime);
   
-  if ((sp = localtime(&now)) == error){
+  struct_pointer = localtime(&now);
+  if (struct_pointer == NULL){
     perror("Error");
-    return 1;
+    return ERROR;
   }
   
   printf("%02d/%d/%02d %02d:%02d:%02d %s\n",
-         sp->tm_mon + 1 , sp->tm_mday,
-         sp->tm_year+1900, sp->tm_hour,
-         sp->tm_min, sp->tm_sec, tzname[sp->tm_isdst]);
-  return 0;
+         struct_pointer->tm_mon + 1 , struct_pointer->tm_mday,
+         struct_pointer->tm_year+1900, struct_pointer->tm_hour,
+         struct_pointer->tm_min, struct_pointer->tm_sec, tzname[struct_pointer->tm_isdst]);
+  return SUCCESSFUL_END;
 }
