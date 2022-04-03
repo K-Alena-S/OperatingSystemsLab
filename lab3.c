60b3154a038d7aba7a0ab1406a94148482856cc1
K-Alena-S <97899604+K-Alena-S@users.noreply.github.com>
2022-04-03 17:13:40 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -6,20 +6,24 @@
 #define ERROR 1
 #define ERROR_CODE_SETUID -1
 
-void print_id() {
-    printf("Real ID = %d\n", getuid());
-    printf("Effective ID = %d\n", geteuid());
+
+void print_id() {  // Печать реального и эффективного идентификатора пользователя
+    printf("Real ID = %d\n", getuid());  //getuid - возвращает реальный идентификатор пользователя (кто запустил процесс).
+    printf("Effective ID = %d\n", geteuid());  // geteuid - возвращает эффективный идентификатор пользователя (кто владеет программой).
 }
 
 int open_file() {
     FILE* file = fopen("text.txt", "r");
-    if (file == NULL) {
-        perror("Opening file");
+    if (file == NULL) { // NULL - код ошибки, если файл не удалось открыть
+        perror("Opening a file");
         return ERROR;
     }
     printf("Fine\n");
     int close_res = fclose(file);
-    if (close_res == EOF) return ERROR;
+    if (close_res == EOF){ // EOF - код ошибки, если файл не удалось закрыть
+        perror("Closing a file");
+        return ERROR;
+    }
     return SUCCESSFUL_END;
 }
 
