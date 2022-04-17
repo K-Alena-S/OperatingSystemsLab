ca373cc89a0a2bb515a632b26b91b609a82f0492
K-Alena-S <97899604+K-Alena-S@users.noreply.github.com>
2022-04-17 15:29:38 +0700

    Create lab9-2.c

---
+++ lab9-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define ERROR_WAIT -1
+#define ERROR_FORK -1
+#define ERROR 1
+#define SUCCESSFUL_END 0
+#define CHILD 0
+
+int main() {
+    int pid = fork();
+    if (pid == ERROR_FORK) {
+        perror("error fork");
+        return ERROR;
+    }
+    if (pid == CHILD) {
+        execlp("cat", "cat", "lab9.txt", NULL);
+        perror("execlp");
+        return ERROR;
+    }
+    int er_wait = wait(NULL);
+    if (er_wait == ERROR_WAIT){
+        perror("error wait");
+        return ERROR;
+    }
+    printf("The line is printed by the ininial parent process\n");
+    return SUCCESSFUL_END;
+}
